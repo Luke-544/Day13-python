43a4c4d508edab280a433aef76d3b3f4bd42015a
Luke-544 <lukesamuels422@gmail.com>
2024-04-03 20:01:34 +0300

    Create day13.py

---
+++ day13.py
@@ -0,0 +1,5 @@
+#nested function calls - they are function calls inside other function calls
+#                        innermost function calls are solved first
+#                        returned value is used as an argument for the next outer function
+
+print(round(abs(float(input("Enter a number: ")))))
